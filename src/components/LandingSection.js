6b646161b48c8b5a81c546e130c68cddd6734a93
Eingest <ak0805@live.de>
2023-07-28 18:08:58 +0200

    modified LandingSection.js

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -15,16 +15,23 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-    <VStack>
-      <Avatar name='Pete' src='https://i.pravatar.cc/150?img=7' />
-      <Heading size="xs">{greeting}</Heading>
-      <br />
-      <Heading>{bio1}</Heading>
-      <Heading>{bio2}</Heading>
-    </VStack>  
-
+    <VStack spacing={16}>
+      <VStack spacing={4} alignItems="center">
+        <Avatar name="Pete" src="https://i.pravatar.cc/150?img=7" size="2xl" />
+        <Heading as="h4" size="md" noOfLines={1}>
+          {greeting}
+        </Heading>
+      </VStack>
+      <VStack spacing={6}>
+        <Heading as="h1" size="3xl" noOfLines={1}>
+          {bio1}
+        </Heading>
+        <Heading as="h1" size="3xl" noOfLines={1}>
+          {bio2}
+        </Heading>
+      </VStack>
+    </VStack>
   </FullScreenSection>
-  
 );
 
 export default LandingSection;
